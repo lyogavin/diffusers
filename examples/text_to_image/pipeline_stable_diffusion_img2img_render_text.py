258c7bab0897141b13428680d24b4f0da18daed8
Yu Li <lyo.gavin@gmail.com>
2023-01-22 14:15:35 -0600

    convert

--- examples/text_to_image/pipeline_stable_diffusion_img2img_render_text.py
+++ examples/text_to_image/pipeline_stable_diffusion_img2img_render_text.py
@@ -33,14 +33,15 @@ def preprocess(image):
         image = [image]
 
     if isinstance(image[0], PIL.Image.Image):
-        w, h = image[0].size
-        w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
+        #w, h = image[0].size
+        #w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
 
-        image = [np.array(i.resize((w, h), resample=PIL_INTERPOLATION["lanczos"]))[None, :] for i in image]
-        image = np.concatenate(image, axis=0)
-        image = np.array(image).astype(np.float32) / 255.0
-        image = image.transpose(0, 3, 1, 2)
-        image = 2.0 * image - 1.0
+        #image = [np.array(i.resize((w, h), resample=PIL_INTERPOLATION["lanczos"]))[None, :] for i in image]
+        #image = np.concatenate(image, axis=0)
+        #image = np.array(image).astype(np.float32) / 255.0
+        #image = image.transpose(0, 3, 1, 2)
+        #image = 2.0 * image - 1.0
+        image = image.convert("RGB")
         image = torch.from_numpy(image)
     elif isinstance(image[0], torch.Tensor):
         image = torch.cat(image, dim=0)
