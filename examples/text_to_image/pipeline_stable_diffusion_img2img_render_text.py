6a02662bfaff88f8908516fa4be813fd9c65c5cd
Yu Li <lyo.gavin@gmail.com>
2023-01-22 14:25:42 -0600

    convert

--- examples/text_to_image/pipeline_stable_diffusion_img2img_render_text.py
+++ examples/text_to_image/pipeline_stable_diffusion_img2img_render_text.py
@@ -40,7 +40,7 @@ def preprocess(image):
         image = [np.array(i.resize((w, h), resample=PIL_INTERPOLATION["lanczos"]))[None, :] for i in image]
         image = np.concatenate(image, axis=0)
         image = np.array(image).astype(np.float32) / 255.0
-        #image = image.transpose(0, 3, 1, 2)
+        image = image.transpose(0, 3, 1, 2)
         image = 2.0 * image - 1.0
         image = torch.from_numpy(image)
     elif isinstance(image[0], torch.Tensor):
